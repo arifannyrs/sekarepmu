fa5df49b7768c7042727440c564389a10a3b5f10
hp <hp@169.254.43.189>
2018-10-25 21:41:06 +0700

    hmmmm

---
+++ src/git/loading.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author Arifannyrs
+ */
+public class loading {
+    private int Nama;
+    
+}
